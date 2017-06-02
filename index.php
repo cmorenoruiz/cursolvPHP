d861b0cfda9574f4bc0443126c9f90fe8a7ee86c
practicasred <practicas.red@gmail.com>
2017-06-02 12:53:35 +0200

    Creación de index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Curso LV</title>
+  </head>
+  <body>
+   <h1>Curso Linux</h1>
+   <h2><a href="phpinfo.php">Prueba PHP</a></h2>
+  </body>
+</html>
+
+
+
